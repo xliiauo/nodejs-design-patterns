a6695b6f81d3c8cdb796dd7626495b5873add183
Xiao Liu <liu.xiao@me.com>
2018-05-06 13:58:12 +0100

    Add comment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Logger from './singleton/Logger'
 
-const logger = new Logger();
-console.log(logger.getInstance().getName());
+// Singleton pattern for a shared logger
+const logger = new Logger().getInstance();
+console.log(logger.getName());
 
 
